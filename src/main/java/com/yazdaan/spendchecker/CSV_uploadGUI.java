44bf6c9ebb9398b636fa9dc17f665dd442d67dd9
YazdaanAli <115666038+YazdaanAli@users.noreply.github.com>
2024-01-25 21:48:15 +0000

    Completed a way in which excel files can be read. IMPROVE CSVuploadgui

--- src/main/java/com/yazdaan/spendchecker/CSV_uploadGUI.java
+++ src/main/java/com/yazdaan/spendchecker/CSV_uploadGUI.java
@@ -8,11 +8,11 @@ import java.io.File;
 public class CSV_uploadGUI extends JFrame {
 	
 	public CSV_uploadGUI() {
-		setTitle("Upload your CSV file");
-		setSize(400, 200);
+		setTitle("Upload your Bank file");
+		setSize(400, 300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JButton uploadButton = new JButton("UPLOAD CSV");
+		JButton uploadButton = new JButton("UPLOAD Bank File");
 		getContentPane().add(uploadButton);
 		
       // Adding Action Listener to Button
@@ -26,6 +26,7 @@ public class CSV_uploadGUI extends JFrame {
                   File selectedFile = fileChooser.getSelectedFile();
                   String selectedFilePath = selectedFile.getAbsolutePath();
                   // Process the selected file
+                  System.out.println(selectedFilePath);
                   System.out.println("Selected file: " + selectedFile.getAbsolutePath());
                   CSV_reader reader = new CSV_reader(selectedFilePath);
                   reader.readCSV();
@@ -40,3 +41,53 @@ public class CSV_uploadGUI extends JFrame {
 	
 	
 }
+
+
+
+
+//package com.yazdaan.spendchecker;
+//
+//import javax.swing.*; //how we will be creating a pop up
+//import java.awt.event.ActionEvent; // when button is clicked this activates the event(ACtion).
+//import java.awt.event.ActionListener; //Listens for the button is clicked. without actino listener action event can not be started
+//import java.io.File;
+//
+//public class CSV_uploadGUI extends JFrame {
+//	
+//	public CSV_uploadGUI() {
+//		setTitle("Upload your CSV file");
+//		setSize(400, 200);
+//		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		
+//		JButton uploadButton = new JButton("UPLOAD CSV");
+//		getContentPane().add(uploadButton);
+//		
+//      // Adding Action Listener to Button
+//      uploadButton.addActionListener(new ActionListener() {
+//          @Override
+//          public void actionPerformed(ActionEvent e) {
+//              JFileChooser fileChooser = new JFileChooser();
+//              fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY); // sets it to only files not directories
+//              fileChooser.setAcceptAllFileFilterUsed(false);
+//              if (fileChooser.showOpenDialog(CSV_uploadGUI.this) == JFileChooser.APPROVE_OPTION) {
+//                  File selectedFile = fileChooser.getSelectedFile();
+//                  String selectedFilePath = selectedFile.getAbsolutePath();
+//                  // Process the selected file
+//                  System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+//                  CSV_reader reader = new CSV_reader(selectedFilePath);
+//                  reader.readCSV();
+//                  
+//              }
+//          }
+//      });
+//      
+//      setVisible(true);
+//			
+//	}
+//	
+//	
+//}
+
+
+
+
--- src/main/java/com/yazdaan/spendchecker/EXCEL_reader.java
+++ src/main/java/com/yazdaan/spendchecker/EXCEL_reader.java
@@ -1,5 +1,42 @@
 package com.yazdaan.spendchecker;
 
-public class EXCEL_reader {
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+public class EXCEL_reader {
+	
+	public List<List<String>> readExcelFile(String filePath) {
+		List<List<String>> data = new ArrayList<>();
+		
+		try (FileInputStream fis = new FileInputStream(new File(filePath));
+	         Workbook workbook = new XSSFWorkbook(fis)) {
+			
+			Sheet sheet = workbook.getSheetAt(0);
+			Iterator<Row> rowIterator = sheet.iterator();
+			
+			while (rowIterator.hasNext()) {
+				Row row = rowIterator.next();
+				List<String> rowData = new ArrayList<>();
+				Iterator<Cell> cellIterator = row.cellIterator();
+				
+				while (cellIterator.hasNext()) {
+					Cell cell = cellIterator.next();
+					rowData.add(cell.toString());
+				}
+				
+				data.add(rowData);
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+				
+		return data;
+	}
 }
