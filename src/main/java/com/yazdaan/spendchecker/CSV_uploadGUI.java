d5905983eb517976662da3e108de1ebb32105475
YazdaanAli <yazdaana109@gmail.com>
2024-01-28 23:12:00 +0000

    Ok so the excel_reader is working know however, I do not understand the way i need to format my code and the classes i need so i will spend next day creeating a uml diagram

--- src/main/java/com/yazdaan/spendchecker/CSV_uploadGUI.java
+++ src/main/java/com/yazdaan/spendchecker/CSV_uploadGUI.java
@@ -4,12 +4,13 @@ import javax.swing.*; //how we will be creating a pop up
 import java.awt.event.ActionEvent; // when button is clicked this activates the event(ACtion).
 import java.awt.event.ActionListener; //Listens for the button is clicked. without actino listener action event can not be started
 import java.io.File;
+import java.util.*;
 
 public class CSV_uploadGUI extends JFrame {
 	
 	public CSV_uploadGUI() {
 		setTitle("Upload your Bank file");
-		setSize(400, 300);
+		setSize(400, 300); 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JButton uploadButton = new JButton("UPLOAD Bank File");
@@ -29,10 +30,19 @@ public class CSV_uploadGUI extends JFrame {
                   if (selectedFilePath.contains(".csv") == true) {
                 	  CSV_reader reader = new CSV_reader(selectedFilePath);
                       reader.readCSV();
-                  }else if (selectedFilePath.contains(".xlsx") == true || selectedFilePath.contains(".xls") == true){
+                  }else if (selectedFilePath.contains(".xlsx") == true){
 //                	  System.out.println("this is when given a .txt");
                 	  EXCEL_reader reader = new EXCEL_reader();
-                	  reader.readExcelFile(selectedFilePath);                	  
+                	  List<List<String>> data = reader.readExcelFile(selectedFilePath);
+                	  
+                	    // Printing data to console
+                	   for (List<String> row : data) {
+                		   for (String cell : row) {
+                	            System.out.print(cell + "\t"); // Tab-separated cells
+                	        }
+                	        System.out.println(); // New line at the end of each row
+                	    }
+                	  
                   }else {
                 	  System.out.println("still works");
                   }
